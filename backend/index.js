aecfd17362a71ab31697cf00542061d431525d9b
ayan878 <ayanraza197@gmail.com>
2024-08-14 23:27:31 +0530

    index

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+const MONGODB_URL = process.env.MONGODB_URL;
+
+console.log("MONGODB_URL:", MONGODB_URL);
+console.log("PORT:", PORT);
+
+mongoose
+  .connect(MONGODB_URL)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Database connection error:", error);
+  });
