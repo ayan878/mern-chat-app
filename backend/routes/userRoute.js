ef6682a05d0e05bc98e2739064dd93e8cad0c44e
ayan878 <ayanraza197@gmail.com>
2024-08-17 01:42:45 +0530

    routes

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,8 +1,15 @@
 import Router from "express";
-import { register } from "../controllers/userController.js";
+import { getOtherUsers, login, logout, register } from "../controllers/userController.js";
+import isAuthenticated from "../middleware/isAuthenticated.js";
 
 const router = Router();
 
 router.post("/register", register);
+router.get("/login", login);
+router.get("/logout", logout);
+router.get("/",isAuthenticated,getOtherUsers);
+
+
+
 
 export default router;
